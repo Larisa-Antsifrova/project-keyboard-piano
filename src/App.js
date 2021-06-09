691849cc59218c2b499d4a0dce74b0761e867f7c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-09 11:12:27 +0300

    Added state to keep current key pressed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from './components/Container/Container';
 import Title from './components/Title/Title';
 import Keyboard from './components/Keyboard/Keyboard';
 import { soundifyKeyboard } from './helpers/soundify-keyboard';
 
 function App() {
+  const [keyCode, setKeyCode] = useState([]);
+
+  const onKeyDown = e => {
+    setKeyCode(prevState => [...prevState, e.code]);
+    soundifyKeyboard(e);
+  };
+
+  const onKeyUp = e => {
+    setKeyCode(prevState => prevState.filter(key => key !== e.code));
+  };
+
   useEffect(() => {
-    window.addEventListener('keydown', soundifyKeyboard);
+    document.addEventListener('keypress', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
 
     return () => {
-      window.removeEventListener('keydown', soundifyKeyboard);
+      document.removeEventListener('keypress', onKeyDown);
+      document.removeEventListener('keyup', onKeyUp);
     };
-  });
+  }, []);
 
   return (
     <Container>
